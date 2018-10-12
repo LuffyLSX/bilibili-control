b90b3945b56d0aa9706131f887da17875bb5c962
LuffyLSX <37948986+LuffyLSX@users.noreply.github.com>
2018-10-12 21:03:00 +0800

    Update B站播放器快捷键.user.js

--- B站播放器快捷键.user.js
+++ B站播放器快捷键.user.js
@@ -1,25 +1,32 @@
 // ==UserScript==
 // @name         B站播放器快捷键
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  alt开关弹幕、enter全屏、crtl+方向键切换集数
-// @author       LuffyLSX
-// @match        *://*.bilibili.com/*
+// @author       Luffy_LSX
+// @match        *://*.bilibili.com/video/*
+// @match        *://*.bilibili.com/bangumi/*
 // ==/UserScript==
 'use strict';
 (function() {
-    $(document).keydown(function(event){
-        if(event.keyCode==18){
+    document.onkeydown=function(event){
+        if(event.keyCode == 18){
             var a = document.getElementsByClassName('bilibili-player-video-btn bilibili-player-video-btn-danmaku')[0].click();
             if(document.getElementsByClassName('bilibili-player-danmaku-setting-lite').length == 1){
                 document.getElementsByClassName('bilibili-player-danmaku-setting-lite')[0].style.display="none";
         }
             var b = document.getElementsByClassName('bui-checkbox')[0].click();
     }
-        if(event.keyCode == 13){
-            var c = document.getElementsByClassName('bilibili-player-video-btn bilibili-player-video-btn-fullscreen video-state-fullscreen-off')[0].click();
+        if(window.location.pathname.search('bangumi') == 1 && event.keyCode == 13){
+            var c = document.getElementsByName('browser_fullscreen')[0].click();
         }
-    });
+        if(document.getElementsByClassName('trynew-btn').length == 1 && window.location.pathname.search('video') == 1 && event.keyCode == 13){
+            document.getElementsByName('browser_fullscreen')[0].click();
+        }
+        else if(window.location.pathname.search('video') == 1 && event.keyCode == 13){
+                document.getElementsByClassName('bilibili-player-iconfont bilibili-player-iconfont-fullscreen-off player-tooltips-trigger')[0].click();
+                }
+    };
 })();
 //切换集数
 (function(){
