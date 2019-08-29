5b30457a072af2151ed1cee9de8e06c22840e62d
Luffy_LSX <754057419@qq.com>
2019-08-29 18:28:45 +0800

    Update B站播放器快捷键.user.js
    更新新版本播放器适配

--- B站播放器快捷键.user.js
+++ B站播放器快捷键.user.js
@@ -6,28 +6,49 @@
 // @author       Luffy_LSX
 // @match        *://*.bilibili.com/video/*
 // @match        *://*.bilibili.com/bangumi/*
+// @match        *://*.bilibili.com/blackboard/*
+// @require      https://code.jquery.com/jquery-2.1.4.min.js
 // ==/UserScript==
-'use strict';
-(function() {
-    document.onkeydown=function(event){
+
+(function(){
+    'use strict';
+    var delay = 2200
+    setTimeout(function(){document.getElementsByClassName('bilibili-player-video-danmaku-input')[0].style.display = "none"}, delay);
+
+
+     //document.getElementsByClassName('bilibili-player-video-danmaku-input')[0].style.display = "block"
+
+    document.onkeydown = function(event){
         if(event.keyCode == 18){
             var a = document.getElementsByClassName('bilibili-player-video-btn bilibili-player-video-btn-danmaku')[0].click();
             if(document.getElementsByClassName('bilibili-player-danmaku-setting-lite').length == 1){
-                document.getElementsByClassName('bilibili-player-danmaku-setting-lite')[0].style.display="none";
         }
             var b = document.getElementsByClassName('bui-checkbox')[0].click();
     }
+        if(document.getElementsByClassName('btn-old').length == 1 && window.location.pathname.search('bangumi') == 1 && event.keyCode == 13){
+            document.getElementsByClassName('bilibili-player-iconfont bilibili-player-iconfont-fullscreen-off player-tooltips-trigger')[0].click();
+        }
         if(window.location.pathname.search('bangumi') == 1 && event.keyCode == 13){
             var c = document.getElementsByName('browser_fullscreen')[0].click();
         }
+
         if(document.getElementsByClassName('trynew-btn').length == 1 && window.location.pathname.search('video') == 1 && event.keyCode == 13){
             document.getElementsByName('browser_fullscreen')[0].click();
         }
         else if(window.location.pathname.search('video') == 1 && event.keyCode == 13){
                 document.getElementsByClassName('bilibili-player-iconfont bilibili-player-iconfont-fullscreen-off player-tooltips-trigger')[0].click();
                 }
+        if(event.keyCode == 96){
+            if( document.getElementsByClassName('bilibili-player-video-danmaku-input')[0].style.display == 'none'){
+
+                document.getElementsByClassName('bilibili-player-video-danmaku-input')[0].style.display = "block"
+            }
+            else{
+                document.getElementsByClassName('bilibili-player-video-danmaku-input')[0].style.display = "none"
+            }
+        }
     };
-})();
+}())
 //切换集数
 (function(){
     var ep = 0;
@@ -60,4 +81,4 @@
             }
         }
     });
-})()
+}())
